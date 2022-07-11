16e3c8a3f21ae0784a2a8ec0b8837ba7e5727589
PathonScript <pathonscript@gmail.com>
2022-07-11 20:53:59 +0700

    background

---
+++ src/components/Title.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Title() {
+  return (
+    <div className="fullflex">
+      <h1 className="text-6xl text-blue-500">Moey 🌊</h1>
+    </div>
+  )
+}
+
+export default Title
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,48 @@
 import type { NextPage } from 'next'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import Head from 'next/head'
+
+import Title from '../components/Title'
+
 
 const Home: NextPage = () => {
   return (
-    <div className="fullflex">
-      <h1 className="text-6xl text-blue-500">Moey 🌊</h1>
+    <>
+      <Head>
+        <title>Moey</title>
+        <meta name="description" content="Moeyeyeyeyeyeyeyeyeyeyeyeyeyeyeyeyeyeyeyeyeyeyeyeyeyeyeyeyeyeey" />
+        <link rel="icon" href="/images/moey.png" />
+      </Head>
 
-    </div>
+      <Parallax pages={4}>
+
+        <ParallaxLayer speed={.8}><Title /></ParallaxLayer>
+
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          factor={1.2}
+          style={{
+            zIndex: -2,
+            backgroundImage: `url(/images/backbeach.png)`,
+            backgroundSize: 'cover',
+          }}
+        />
+
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          factor={1.2}
+          style={{
+            zIndex: -2,
+            backgroundImage: `url(/images/frontlayer.png)`,
+            backgroundSize: 'cover',
+          }}
+        />
+
+      </Parallax>
+
+    </>
   )
 }
 

293bead3c7a0e1868b2205cb63a7113eabe93baa
PathonScript <pathonscript@gmail.com>
2022-07-11 20:28:34 +0700

    Project Setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,7 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <div className="fullflex">
-      <h1 className="text-6xl text-blue-500">NextJS + TailwindCSS🍃</h1>
-      <h1 className="text-5xl text-blue-400">Template</h1>
+      <h1 className="text-6xl text-blue-500">Moey 🌊</h1>
 
     </div>
   )
